53d5f01798ab898d5f364a378f5c28f94dd85f39
Syzygianinfern0 <spsharan2000@gmail.com>
2024-01-06 02:14:04 -0600

    Plot

--- metrics/table_3.py
+++ metrics/table_3.py
@@ -10,8 +10,7 @@ GPT-3	22.33	75.18	73.72	81.02	56.93	31.77	99.96	100
 GPT-3-ASSIST-PAR	92.82	98.55	97.1	99.28	94.2	89.15	99.86	99.28
 """
 
-import pandas as pd
-import plotly.express as px
+import plotly.graph_objects as go
 
 closed_loop_non_reactive_metrics = {
     "GPT-3": [18.08, 63.04, 60.14, 78.26, 57.97, 31.3, 99.93, 98.91],
@@ -34,19 +33,36 @@ metric_names = [
     "Direction",
 ]
 
-df = pd.DataFrame.from_dict(closed_loop_non_reactive_metrics, orient="index")
-df = df.transpose()
-df.columns = ["GPT-3", "GPT-3-ASSIST-PAR"]
-df["Metric"] = metric_names
-df = df.melt(id_vars=["Metric"], var_name="Method", value_name="Value")
-
-fig = px.line_polar(
-    df,
-    r="Value",
-    theta="Metric",
-    color="Method",
-    line_close=True,
-    color_discrete_sequence=px.colors.sequential.Plasma_r,
-    template="plotly_dark",
+fig = go.Figure()
+fig.add_trace(
+    go.Scatterpolar(
+        r=closed_loop_non_reactive_metrics["GPT-3"][1:],
+        theta=metric_names[1:],
+        fill="toself",
+        name="GPT-3",
+    )
 )
+fig.add_trace(
+    go.Scatterpolar(
+        r=closed_loop_non_reactive_metrics["GPT-3-ASSIST-PAR"][1:],
+        theta=metric_names[1:],
+        fill="toself",
+        name="GPT-3-ASSIST-PAR",
+    )
+)
+
+fig.update_layout(
+    title="Closed Loop Non-Reactive Metrics",
+    polar=dict(radialaxis=dict(visible=True, range=[0, 100])),
+    showlegend=True,
+)
+
+# fig.update_layout(
+#     title={"text": "Closed Loop Non-Reactive Metrics", "x": 0.5, "xanchor": "center", "yanchor": "top"},
+#     polar=dict(radialaxis=dict(visible=True, range=[0, 100])),
+#     legend=dict(x=0.5, y=-0.1, xanchor="center", yanchor="top"),
+#     showlegend=True,
+#     template="presentation",
+# )
+
 fig.show()
